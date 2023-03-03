b891a52438f50c23d28feaa5480992fffeb91e00
Irirwanirira <irijoseph41@gmail.com>
2023-03-03 08:29:51 +0200

    Dsiplay api to the UI

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import Greeting from './Greeting';
+
+const App = () => (
+  <div>
+    <Routes>
+      <Route path="/greeting" element={<Greeting />} />
+    </Routes>
+  </div>
+);
+
+export default App;
